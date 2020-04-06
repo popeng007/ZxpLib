3d6f23772324b37866027150c68df62e5243c65b
popeng007 <popeng007@126.com>
2020-04-06 14:28:57 +0800

    init && dev package

---
+++ src/Hello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Hello World
+ * 
+ * @author zxp <zhang.xian.peng@qq.com>
+ * @date 2020-04-06
+ */
+namespace ZxpLib;
+
+class Hello
+{
+    public static function world()
+    {
+        echo 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use ZxpLib\Hello;
+echo 'test: ';
+echo Hello::world();
